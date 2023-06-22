2f541f9be8f635ddebf6b2be93a01ecfd3c9979e
Gauri <Gauri@DESKTOP-LRKRLSD>
2023-06-22 09:26:37 +0530

    New added

--- BulkyBookWeb/wwwroot/js/product.js
+++ BulkyBookWeb/wwwroot/js/product.js
@@ -26,7 +26,7 @@ function loadDataTable()
                          <div class="w-75 btn-group" role="group">
                         <a href="/Admin/Product/Upsert?id=${data}"
                         class="btn btn-primary mx-2"><i class="bi bi-pencil-square"></i> Edit</a>
-                        <a 
+                        <a onclick=Delete('/Admin/Product/DeletePOST/${data}')
                         class="btn btn-danger mx-2"><i class="bi bi-trash"></i> Delete</a>
                     </div>
 
@@ -39,7 +39,34 @@ function loadDataTable()
     });
 }
 
-function Delete(utl)
+function Delete(url)
 {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                url: url,
+                type: 'DELETE',
+                success: function (data)
+                {
+                    if (data.success) {
+                        dataTable.ajax.reload();
+                        toastr.success(data.message);
+                    }
+                    else
+                    {
+                        toastr.error(data.message);
+                    }
+                }
+            })
+        }
+    })
 
 }
